20307d1e4ead185904beb31b5d66419f6eea58fc
Muhammad Usama <89479745+rmusamam@users.noreply.github.com>
2022-08-13 20:21:25 +0500

    email already exist hash n salt part has been done in this commit

--- Controller/accountController.js
+++ Controller/accountController.js
@@ -1,11 +1,25 @@
 const userAccount = require('../Model/accountSchema')
+const bcrypt=require('bcryptjs')
 
 
 exports.account= async(req,res)=>{
     const data=req.body
 
     const emailExist=await userAccount.findOne({email:data.email})
+    if(emailExist)
+    {
+        console.log("email already axist")
+        res.status(400).send("Email already exists")
+    }
 
+    const salt=await bcrypt.genSalt(10)
+
+    const hashedPassword=await bcrypt.hash(data.password,salt)
+
+    
+
+    
+    
 
     console.log('this is my req.body',data)
     res.send("registeration partially done")
